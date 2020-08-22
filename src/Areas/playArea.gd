bf0c55268d6e82cf38b236afa687a6fced352372
Elena <elena.bartaskaite@gmail.com>
2020-08-22 21:23:35 +0300

    drag and drop works

---
+++ src/Areas/playArea.gd
@@ -0,0 +1,6 @@
+extends RigidBody2D
+
+
+
+func _on_Card_dragsignal(extra_arg_0):
+	pass # Replace with function body.
--- src/drag_drop.gd
+++ src/drag_drop.gd
@@ -1,31 +1,28 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-var dragging = false
+signal clicked
 
-signal dragsignal;
+var held = false
 
-func _ready():
-	connect("dragsignal",self,"_set_drag_pc")
-	
-	
-func _process(delta):
-	if dragging:
-		var mousepos = get_viewport().get_mouse_position()
-		self.position = Vector2(mousepos.x, mousepos.y)
-
-		
-
-func _set_drag_pc():
-	dragging=!dragging
-
-
-func _on_Card_input_event(viewport, event, shape_idx):
+func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			emit_signal("dragsignal")
-		elif event.button_index == BUTTON_LEFT and !event.pressed:
-			emit_signal("dragsignal")
-	elif event is InputEventScreenTouch:
-		if event.pressed and event.get_index() == 0:
-			self.position = event.get_position()
+			emit_signal("clicked", self)
 
+
+func _physics_process(delta):
+	if held:
+		global_transform.origin = get_global_mouse_position()
+
+
+func pickup():
+	if held:
+		return
+	mode = RigidBody2D.MODE_STATIC
+	held = true
+
+func drop(impulse=Vector2.ZERO):
+	if held:
+		mode = RigidBody2D.MODE_RIGID
+		apply_central_impulse(impulse)
+		held = false
